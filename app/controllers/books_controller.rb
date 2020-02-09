029afb5f830915d15b6ba56d371c7308218f7ba6
lefokd <llllivagin0528@gmail.com>
2020-02-09 08:26:53 +0000

    complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,7 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to @book, notice: 'Book was successfully created.'
     else
+      @books = Book.all
       render :index
     end
   end
